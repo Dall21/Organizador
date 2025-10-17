9732137d4ef4cde26c919a1e03c1675a65b56bc0
Diego <macbook@Macbooks-MacBook-Pro.local>
2025-10-17 03:18:37 -0600

    update profile

--- app/Views/Profile.blade.php
+++ app/Views/Profile.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
 <h1>Hola {{ session()->get('name') }} aqui podras editar tu perfil</h1>
+<h1>Name:</h1>
+<h2>{{ session()->get(key: 'name') }}</h2>
+<h1>Email:</h1>
+<h2>{{ session()->get('email') }}</h2>
+<div class="column">
+    <input type="image" src="{{ session()->get('image_path') ?? base_url('assets/default_avatar.jpg') }}" alt="">
+<button onclick="">Edit Photo</button>
+<button>Delte Photo</button>
+</div>
+<hr>
+<button >Edit Profile</button>
+<button>Change password</button>
 @endsection 
